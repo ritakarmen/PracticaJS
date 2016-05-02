d6a8517d3745575f8fea80ff65bd11f40ce7c61c
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-02 01:26:19 -0500

    Added files via upload

--- Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
+++ Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
@@ -1,1 +1,25 @@
 //Escribe aquí tú código
+// function recaudo(){
+// 	var c=parseInt(prompt("Ingresa el número de sucursales:"));
+// 	var t=parseInt(prompt("Ingresa el número de tiendas en cada sucursal:"));
+// 	var n=parseInt(prompt("Ingresa el número de empleados que trabajan en cada tienda:"));
+// 	var vTienda=0;
+// 	var vSucursal=0;
+// 	for(i=1;i<=c;i++)
+// 	for(j=1;j<=t;j++)
+// 	for(k=1;k<=n;k++){
+// 		var vEmpleado=parseInt(prompt("Ingresa la venta del empleado "+k+" de la tienda "+j+" y sucursal "+i));
+// 		document.write("La venta del empleado "+k+" de la tienda "+j+" y sucursal "+i+" fue de s/."+vEmpleado+'<br>');
+// 	}
+// }
+
+// recaudo();
+var sucursal = parseInt(prompt('ingrese el numero de sucursales '))
+var tienda = parseInt(prompt('ingrese el numero de tiendas '))
+var empleado = parseInt(prompt('ingrese el numero de empleados '))
+for (var i = 1; i <= sucursal; i++)
+for (var j = 1; j <= tienda; j++)
+for (var k = 1; k <= empleado; k++) {
+	var venta = parseInt(prompt('ingrese la venta realizada por día '+ k +'de la tienda ' + j +' y sucursal '+i ))
+	document.write("La venta del empleado "+k+" de la tienda "+j+" y sucursal "+i+" fue de s/."+venta+'<br>');
+};
--- Ejercicio 9 - Mensualidades/Problema/main.js
+++ Ejercicio 9 - Mensualidades/Problema/main.js
@@ -1,1 +1,30 @@
 //Escribe aquí tú código
+var opcion = ['Monto por mes','Suma total de pago']
+var mensaje = 'Elija la opcion que desee ejecutar \n'
+var opciones = opcion.length
+for (var i =0; i < opciones; i++) {
+	mensaje+=(i+1)+'. '+opcion[i]+'\n';
+};
+do{
+	var seleccion=parseInt(prompt(mensaje))
+}while(seleccion<=0 || seleccion>=3)
+for (var i = seleccion-1; i <opciones; i++) {
+	if (i == 0) {
+		mensaje = parseInt(prompt('ingrese el numero de mes, para saber el monto a pagar '));
+		var opera = Math.pow(2,mensaje-1)*10;
+		alert('El monto a pagar por este mes sera '+opera)
+	}
+	else if (i == 1) {
+		var resultadoFinal=0; 
+		var a= 10;
+		var mes=0;
+		mensaje2 = parseInt(prompt('ingrese el numero de meses que realizo el pago '));
+		for (var j= 1; j <=mensaje2; j++) {
+			mes= mes+a;
+			a=mes;
+			resultadoFinal= resultadoFinal+mes;
+		};
+		
+		alert("La suma total de pago es: "+resultadoFinal);
+	}
+}
