545e2673ace19823224eb1d30549a1dbb189dab5
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-03 16:13:40 -0500

    Added files via upload

--- Ejercicios estructuras/Ejercicio8 - Tipo Triangulo/Problema/main.js
+++ Ejercicios estructuras/Ejercicio8 - Tipo Triangulo/Problema/main.js
@@ -1,10 +1,14 @@
 // Escribe aquí tu codigo Javascript
 var mensaje = [parseInt(prompt('ingrese medida1: ')),parseInt(prompt('ingrese medida2: ')),parseInt(prompt('ingrese medida3: '))]
-var triangulo1 = 2*(mensaje[0]+mensaje[1]+mensaje[2])>(mensaje[0]+mensaje[1]+mensaje[2]);
-// var triangulo2 = mensaje[1]+mensaje[2]>mensaje[0];
-// var triangulo3 = mensaje[2]+mensaje[0]>mensaje[1];
-if (triangulo1=true) {
+if ((mensaje[0]+mensaje[1])>mensaje[2]>(mensaje[0]-mensaje[1])&&(mensaje[1]+mensaje[2])>mensaje[0]>(mensaje[1]-mensaje[2])&&(mensaje[0]+mensaje[2])>mensaje[1]>(mensaje[0]-mensaje[2])&&(mensaje[1]+mensaje[0])>mensaje[2]>(mensaje[1]-mensaje[0])&&(mensaje[2]+mensaje[1])>mensaje[0]>(mensaje[2]-mensaje[1])&&(mensaje[2]+mensaje[0])>mensaje[1]>(mensaje[2]-mensaje[0])) {
 	alert('El triangulo existe')
+	if(mensaje[0]==mensaje[1] && mensaje[2]==mensaje[1]){
+				alert('Es un triangulo Equilatero')
+			}else if (mensaje[0]==mensaje[1]||mensaje[2]==mensaje[1]||mensaje[0]==mensaje[2]  ){
+				alert('Es un triangulo Isosceles')
+			}else{
+				alert('Es un triangulo Escaleno')
+			}
 }else{
 	alert('El triangulo NO existe')
 }
