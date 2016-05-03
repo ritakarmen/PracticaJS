6014759d9f1789c5b52d334afcb08997860b5810
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-03 17:47:14 -0500

    Added files via upload

--- Ejercicios estructuras/Ejercicio9 - Traducir/Problema/main.js
+++ Ejercicios estructuras/Ejercicio9 - Traducir/Problema/main.js
@@ -1,4 +1,11 @@
 // Escribe aquí tu codigo Javascript
-
-
-
+var palabra;
+palabra = prompt ('Por favor introduce la palabra  a traducir:');
+if (palabra =="") { alert ('No has introducido ninguna la palabra'); }
+        switch (palabra) {
+            case 'casa': alert ("house"); break;
+            case 'mesa': alert ("table"); break;
+            case 'perro': alert ("dog"); break;
+            case 'gato': alert ("cat"); break;
+            default: alert ("Solo puedo traducir las palabras indicadas"); break;
+        }
