6fbcb43c6ea2bf9bde5b0d32563ea725bb44a2bc
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-03 18:07:04 -0500

    Added files via upload

--- Ejercicios estructuras/Ejercicio9 - Traducir/Problema/main.js
+++ Ejercicios estructuras/Ejercicio9 - Traducir/Problema/main.js
@@ -1,11 +1,20 @@
 // Escribe aquí tu codigo Javascript
 var palabra;
 palabra = prompt ('Por favor introduce una de estas palabras "casa, mesa, perro o gato" para traducir:');
-if (palabra =="") { alert ('No has introducido ninguna la palabra'); }
+if (palabra =="") { alert ('No has introducido ninguna palabra'); }
         switch (palabra) {
-            case 'casa': alert ("house"); break;
-            case 'mesa': alert ("table"); break;
-            case 'perro': alert ("dog"); break;
-            case 'gato': alert ("cat"); break;
-            default: alert ("Solo puedo traducir las palabras indicadas"); break;
+            case 'casa': 
+            	alert ("house"); 
+            break;
+            case 'mesa':
+            	alert ("table"); 
+            break;
+            case 'perro': 
+            	alert ("dog"); 
+            break;
+            case 'gato': 
+            	alert ("cat"); 
+            break;
+            default: alert ("Solo puedo traducir las palabras indicadas"); 
+            break;
         }
