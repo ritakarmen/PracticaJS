d2cf662a50646b4bffc1a09b737c2c05bd1c79a9
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-04 15:58:46 -0500

    Added files via upload

--- Ejercicios funciones/Ejercicio4 - Mayusculas o minusculas/Problema/main.js
+++ Ejercicios funciones/Ejercicio4 - Mayusculas o minusculas/Problema/main.js
@@ -1,4 +1,13 @@
 // Escribe aquí tu codigo Javascript
-
-
-
+function mayusMin(){
+	var mensaje=prompt('Ingresa una oracion, ya sea en mayusculas o minusculas: ')
+	var mostrar=mensaje.toString()
+	if (mostrar==mensaje.toUpperCase()) {
+	alert('La oracion: '+mostrar+' esta escrita en mayuscula')
+	}else if(mostrar==mensaje.toLowerCase()){
+		alert('La oracion: '+mostrar+' esta escrita en minuscula')
+		}else{
+			alert('La oracion: '+mostrar+' esta escrita en mayuscula y minuscula')
+			};
+}
+mayusMin()
