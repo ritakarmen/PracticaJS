30c1622c9188829b5f771eb45e9361930bef4daa
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-04 15:13:03 -0500

    Added files via upload

--- Ejercicios funciones/Ejercicio2 - Par o impar/Problema/main.js
+++ Ejercicios funciones/Ejercicio2 - Par o impar/Problema/main.js
@@ -1,4 +1,14 @@
 // Escribe aquí tu codigo Javascript
+function par_impar(numero) {
+  numero=parseInt(prompt('Ingrese el numero '))
+  resultado = numero%2;
 
-
-
+  if(resultado==0){
+  	alert("El residuo es " + resultado+' por lo tanto es par');
+	}else{
+		alert("El residuo es " + resultado+' por lo tanto es impar');
+	}
+}
+ 
+var resultado;
+par_impar();
--- Ejercicios funciones/Ejercicio3 - Ordenando numeros/Problema/main.js
+++ Ejercicios funciones/Ejercicio3 - Ordenando numeros/Problema/main.js
@@ -1,4 +1,5 @@
 // Escribe aquí tu codigo Javascript
-
-
-
+	var lista=[parseInt(prompt('Ingrese el primer numero ')),parseInt(prompt('Ingrese el segundo numero ')),parseInt(prompt('Ingrese el tercer numero '))]
+	lisstas=lista.toString();
+	ordenado=lista.sort(function(a,b){return b-a});
+	 alert('Los numeros ingresados son: '+lisstas+'\n'+'Estos son los numeros ordenados de mayor a menor: '+ordenado);
