e4f6dbaafc827c006c074ebf0d1dfc5de951e0a3
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-06 02:56:05 -0500

    Added files via upload

--- Ejercicios funciones/Ejercicio10- Cantidad de digitos/Problema/main.js
+++ Ejercicios funciones/Ejercicio10- Cantidad de digitos/Problema/main.js
@@ -1,4 +1,21 @@
 // Escribe aquí tu codigo Javascript
+function cantidadDigitos(a){
+	var conteoDigito;
+	if (a>0) {// paara numero positivos
+		conteoDigito='Numero de digitos es: '+a.toString().length;//convierte la cifra a string para poder ser contada
+	}else{
+		conteoDigito='El numero ingresado no es un numero entero positivo'
+	}
+	return conteoDigito;
+}
+var numero=parseInt(prompt('Introducir un numero entero positivo'))
+alert(cantidadDigitos(numero))
 
+/*CANTIDAD DE DIGITOS PARA TODO TIPO DE NUMERO
+function cantidadDigitos(){
+var variable=prompt('Ingrese un numero entero positivo')
+var digitos=variable.length
+alert(digitos)
+}
 
-
+cantidadDigitos()*/
--- Ejercicios funciones/Ejercicio9 - Multiplo/Problema/main.js
+++ Ejercicios funciones/Ejercicio9 - Multiplo/Problema/main.js
@@ -1,18 +1,30 @@
 // Escribe aquí tu codigo Javascript
-function multiplo(a,b){
+function multiplos(a){
+	var numero = "";
+	for(var i=1; i<11;i++){
+		numero +=  i*a + " ";
+	}
+	return numero;
+}
+var a = parseInt(prompt("Ingrese el numero a determinar sus multiplos:"));
+alert('Los 10 primeros multiplos de: '+a+'\n'+ 'son '+multiplos(a))
+
+
+// Multiplos de un  numero solo multiplos menores a 10
+/*function esmultiplo(a,b){
 	var numero=a;
 	var numero2=b;
 	var respuesta= numero%numero2
 	if (respuesta==0) {
-		// return true
-		alert('siii')
+		return true
+
 	}else{
-		// return false//multiplo(numero,multiplos)
-		alert('noooo')
+		return false//multiplo(numero,multiplos)
+
 	}
 }
 
-// function multiploss(){
+function multiploss(){
 var numero=parseInt(prompt('Ingrese un numero que desee sus multiplos'))
 var multiplos=[];
 	for (var i = 1; i <=10; i++) {
@@ -21,8 +33,8 @@ var multiplos=[];
 		};
 	};
 	alert(multiplos)
-// }
+ }
 
 
-// multiploss()
+ multiploss()*/
 
