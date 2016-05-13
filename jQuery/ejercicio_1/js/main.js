2299b2b020d3cb07fcba01eedb2271f76502bb7a
CarmenRita <ritacarmenortiz@gmail.com>
2016-05-13 11:35:30 -0500

    Add files via upload

--- jQuery/ejercicio_1/js/main.js
+++ jQuery/ejercicio_1/js/main.js
@@ -1,29 +1,45 @@
 $(document).ready(function(){
 	var player =1;
-	$('.square').on('click',function(event){
+	$('.caja').on('click',function(event){
 
-		var squareSelected = $(this);
-		if (squareSelected.hasClass('ex')||squareSelected.hasClass('oh')) {
+		var cajaSelected = $(this);
+		if (cajaSelected.hasClass('fa fa-times')||cajaSelected.hasClass('fa fa-circle-o')) {
 			alert('Este cuadro ya ha sido seleccionado, seleccione otro. ')
 		}else{
 			if(player === 1){
-				squareSelected.addClass('ex');
+				cajaSelected.addClass('fa fa-times');
+				if (checkIfPlayerWon('fa fa-times')) {
+					alert('GANOOO el jugador '+player);
+				}else{
 				player = 2;
+				}
 			}else{
-				squareSelected.addClass('oh');
+				cajaSelected.addClass('fa fa-circle-o');
+				if (checkIfPlayerWon('fa fa-circle-o')) {
+					alert('GANOOO el jugador '+player);
+				}else{
 				player = 1;
+				}
 			}
 		}
 	})
 	function checkIfPlayerWon(symbol){
-		if ($('.sq1').hasClass(symbol)&& $('.sq2').hasClass(symbol)&& $('.sq3').hasClass(symbol)) {
+		if ($('.cj1').hasClass(symbol)&& $('.cj2').hasClass(symbol)&& $('.cj3').hasClass(symbol)) {
 			return true;
-		}else if ($('.sq4').hasClass(symbol)&& $('.sq5').hasClass(symbol)&& $('.sq6').hasClass(symbol)) {
+		}else if ($('.cj4').hasClass(symbol)&& $('.cj5').hasClass(symbol)&& $('.cj6').hasClass(symbol)) {
 			return true;
-		}else if ($('.sq7').hasClass(symbol)&& $('.sq8').hasClass(symbol)&& $('.sq9').hasClass(symbol)) {
+		}else if ($('.cj7').hasClass(symbol)&& $('.cj8').hasClass(symbol)&& $('.cj9').hasClass(symbol)) {
 			return true;
-		}else{
+		}else if ($('.cj1').hasClass(symbol)&& $('.cj4').hasClass(symbol)&& $('.cj7').hasClass(symbol)){
 			return false;
+		}else if ($('.cj2').hasClass(symbol)&& $('.cj5').hasClass(symbol)&& $('.cj8').hasClass(symbol)){
+			return true;
+		}else if ($('.cj3').hasClass(symbol)&& $('.cj6').hasClass(symbol)&& $('.cj9').hasClass(symbol)) {
+			return true;
+		}else if ($('.cj3').hasClass(symbol)&& $('.cj5').hasClass(symbol)&& $('.cj7').hasClass(symbol)) {
+			return true
+		}else if ($('.cj1').hasClass(symbol)&& $('.cj5').hasClass(symbol)&& $('.cj9').hasClass(symbol)) {
+			return true
 		}
 	}
 })
